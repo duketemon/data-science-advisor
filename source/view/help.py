9b9715ff0b13b66cf0cb185faf26cdd5be617265
duketemon <duketemon@gmail.com>
2020-06-24 16:15:11 +0200

    Updated a bot description

--- source/view/help.py
+++ source/view/help.py
@@ -3,7 +3,7 @@ from ._ui_symbols import get_new_line_point_symbol
 
 def get_help_view():
     return '\n'.join([
-        "Hey! I'm an {NAME} bot. I can",
+        "Hey! I'm a Data Science Advisor. I can",
         f"{get_new_line_point_symbol()} Recommend online courses - /online_courses",
         "If you have any ideas, please visit the repository of the project."
     ])
