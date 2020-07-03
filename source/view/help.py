c31ff4a3e6e206898cb31d00447a96740d83b20c
duketemon <duketemon@gmail.com>
2020-07-03 14:51:35 +0200

    Updated the "help view"

--- source/view/help.py
+++ source/view/help.py
@@ -1,10 +1,8 @@
-from ._ui_symbols import get_new_line_point_symbol
-
-
 def get_help_view():
     return '\n'.join([
-        "Hey! I'm a Data Science Advisor. I can",
-        f"{get_new_line_point_symbol()} Recommend online courses - /online_courses",
-        f"{get_new_line_point_symbol()} Recommend platforms to practice your skills - /practice_platforms",
-        "If you have any ideas, please visit the repository of the project (https://github.com/duketemon/data-science-advisor)."
+        "Hey! I'm a Data Science Advisor. You can control me by sending these commands:\n",
+        "/online_courses - recommend online courses",
+        "/practice_platforms - recommend platforms to practice your skills",
+        "/essential_skills - list of essential skills for a position",
+        "\nIf you have any ideas, please visit the repository of the project (https://github.com/duketemon/data-science-advisor)"
     ])
