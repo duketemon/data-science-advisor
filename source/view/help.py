d633710d58364c7c578d264e99605f39a99c4804
duketemon <duketemon@gmail.com>
2020-07-05 23:40:40 +0200

    Updated the `get_help` function

--- source/view/help.py
+++ source/view/help.py
@@ -1,7 +1,8 @@
 def get_help_view():
     return '\n'.join([
         "Hey! I'm a Data Science Advisor. You can control me by sending these commands:\n",
-        "/online_courses - recommend online courses",
+        "/courses - recommend courses (theory + practice)",
+        "/lectures - recommend lectures (only theory)",
         "/practice_platforms - recommend platforms to practice your skills",
         "/essential_skills - list of essential skills for a position",
         "\nIf you have any ideas, please visit the repository of the project (https://github.com/duketemon/data-science-advisor)"
