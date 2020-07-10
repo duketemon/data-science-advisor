93c98ef420ab43ad3eb4b6279c3ab5c808b02724
duketemon <duketemon@gmail.com>
2020-07-10 15:22:09 +0200

    Updated the /practice_platforms command

--- source/data/practice_platforms.py
+++ source/data/practice_platforms.py
@@ -2,21 +2,26 @@ from .data_storage import get_storage
 
 
 storage = get_storage()
-data = storage.knowledge_base.find_one({'name': 'practice_platforms'})
+
+
+def __get_data():
+    return storage.knowledge_base.find_one({'name': 'practice_platforms'})
 
 
 def get_topics():
-    return data['topics'] + ['All']
+    data = __get_data()
+    return list(data['data'].keys()) + ['All']
 
 
 def get_platforms(topic: str):
+    data = __get_data()
     platforms = None
     topics = get_topics()
     if topic in topics:
         if topic == 'All':
             platforms = []
             for topic in topics[:-1]:
-                platforms.extend(data['platforms'][topic])
+                platforms.extend(data['data'][topic])
         else:
-            platforms = data['platforms'][topic]
+            platforms = data['data'][topic]
     return platforms
